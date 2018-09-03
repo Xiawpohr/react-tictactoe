e70283a6814e68318659ba049f3c68eac44d5db0
XiawPohr <paradise121329@gmail.com>
2018-09-03 15:08:06 +0800

    Declaring Winner

--- src/index.js
+++ src/index.js
@@ -13,6 +13,26 @@ function Square (props) {
   )
 }
 
+function calculateWinner (squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null
+}
+
 class Board extends React.Component {
   constructor (props) {
     super(props)
@@ -24,6 +44,7 @@ class Board extends React.Component {
 
   handleClick (pos) {
     const squares = this.state.squares.slice()
+    if (calculateWinner(squares) || squares[pos]) return
     squares[pos] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       squares,
@@ -41,7 +62,13 @@ class Board extends React.Component {
   }
 
   render () {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+    const winner = calculateWinner(this.state.squares)
+    let status
+    if (winner) {
+      status = 'Winner: ' + winner
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+    }
 
     return (
       <div>
