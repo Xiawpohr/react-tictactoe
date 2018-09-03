2cd6bbfc4ecfebed0c954816629a08a6ab35bf64
XiawPohr <paradise121329@gmail.com>
2018-09-03 14:07:14 +0800

    use props

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import './index.css'
 class Square extends React.Component {
   render () {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className='square'>
+        {this.props.value}
       </button>
     )
   }
@@ -14,7 +14,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare (i) {
-    return <Square />
+    return <Square value={i} />
   }
 
   render () {
@@ -22,18 +22,18 @@ class Board extends React.Component {
 
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
+        <div className='status'>{status}</div>
+        <div className='board-row'>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
-        <div className="board-row">
+        <div className='board-row'>
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
-        <div className="board-row">
+        <div className='board-row'>
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
@@ -46,11 +46,11 @@ class Board extends React.Component {
 class Game extends React.Component {
   render () {
     return (
-      <div className="game">
-        <div className="game-board">
+      <div className='game'>
+        <div className='game-board'>
           <Board />
         </div>
-        <div className="game-info">
+        <div className='game-info'>
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
         </div>
